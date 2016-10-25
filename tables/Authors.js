665ed58ca910fd0d3a1f1e6c33344bc9712b1594
Verónica Cordobés <verocordobes@hotmail.com>
2016-10-25 13:26:01 +0200

    Anadimos id de news

--- tables/Authors.js
+++ tables/Authors.js
@@ -7,9 +7,10 @@ var azureMobileApps = require('azure-mobile-apps');
 var table = azureMobileApps.table();
 
 table.columns = {
-    "author" : "string",
-    "title" : "string",
-    "text" : "string"
+    "id": "string".
+    "author": "string",
+    "title": "string",
+    "text": "string"
 };
 
 module.exports = table;
