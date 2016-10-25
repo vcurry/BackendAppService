e8db814af5546d64a0701eaac9c86ba9cdf37628
Verónica Cordobés <verocordobes@hotmail.com>
2016-10-25 12:28:06 +0200

    Anadimos api

---
+++ api/readAllRecords.js
@@ -0,0 +1,27 @@
+/**
+ * Created by veronicacordobes on 25/10/16.
+ */
+var api = {
+    get: function (req, res, next) {
+
+        if (typeof req.params.length < 0) {
+            return next();
+        }
+
+        var context = req.azureMobile;
+        var user = context.user.id;
+
+        var query = {
+            sql: "Select * FROM Authors"
+        };
+
+        req.azureMobile.data.execute(query)
+            .then(function (result) {
+                res.json(result);
+            });
+    }
+
+};
+
+//api.get.access = 'authenticated';
+module.exports = api;
